dfa45b649379a82ed4230e71e20ea9f78e270882
Brandon Franks <bgfranks.dev@gmail.com>
2022-07-21 18:58:54 -0400

    set up the next router to pull the query from the text input and redirect to the page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
 import Head from 'next/head'
 
@@ -7,6 +9,18 @@ import { SearchIcon, MicrophoneIcon } from '@heroicons/react/solid'
 import Footer from '../components/Footer'
 
 export default function Home() {
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+  const [searchState, setSearchState] = useState('')
+
+  const search = (e) => {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+    if (!term.trim()) return
+
+    router.push(`/search?term=${term.trim()}`)
+  }
+
   return (
     <div>
       <Head>
@@ -25,14 +39,21 @@ export default function Home() {
           height='100'
           objectFit='cover'
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
+          alt='Google Logo'
         />
         <div className='flex w-full max-w-[90%] mt-5 justify-center items-center border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full sm:max-w-screen-xl lg:max-w-2xl'>
           <SearchIcon className='h-5 text-gray-500 mr-3' />
-          <input className='flex-grow focus:outline-none' type='text' />
+          <input
+            ref={searchInputRef}
+            className='flex-grow focus:outline-none'
+            type='text'
+          />
           <MicrophoneIcon className='h-5' />
         </div>
         <div className='flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
-          <button className='btn'>Google Search</button>
+          <button className='btn' onClick={search}>
+            Google Search
+          </button>
           <button className='btn'>I'm Feeling Lucky</button>
         </div>
       </form>
